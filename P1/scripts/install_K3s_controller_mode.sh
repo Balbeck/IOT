35921510255ca1dc20c893e70b3f3c78c8eeb140
Hector <90897873+Balbeck@users.noreply.github.com>
2026-02-10 01:33:54 +0100

    Refactor K3s installation script for controller mode
    Updated the script to install K3s in controller mode with specific node IP and token file path. Added curl installation and improved update commands.

--- P1/scripts/install_K3s_controller_mode.sh
+++ P1/scripts/install_K3s_controller_mode.sh
@@ -1,2 +1,24 @@
-sudo apt-get update
-sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms
+#!/bin/bash
+
+NODE_IP="192.168.56.110"
+TOKEN_FILE="/vagrant_shared/node-token"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y curl
+# sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms
+
+curl -sfL https://get.k3s.io | \
+	K3S_KUBECONFIG_MODE="644" \
+	INSTALL_K3S_EXEC="server \
+		--node-ip $NODE_IP \
+            	--advertise-address $NODE_IP \
+            	--tls-san $NODE_IP \
+            	--flannel-iface eth1" \
+        sh -
+
+until [ -f /var/lib/rancher/k3s/server/node-token ]; do
+    sleep 3
+done
+
+sudo tee "$TOKEN_FILE" < /var/lib/rancher/k3s/server/node-token >/dev/null
