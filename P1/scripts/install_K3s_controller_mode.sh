091bcf64f3511a5b45143772eb237f9f472386f2
balbecke <albecker.hector@gmail.com>
2026-02-10 02:53:22 +0100

    feat: p1 Clean Working!

--- P1/scripts/install_K3s_controller_mode.sh
+++ P1/scripts/install_K3s_controller_mode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 NODE_IP="192.168.56.110"
 TOKEN_FILE="/vagrant_shared/node-token"
 
@@ -12,13 +13,14 @@ curl -sfL https://get.k3s.io | \
 	K3S_KUBECONFIG_MODE="644" \
 	INSTALL_K3S_EXEC="server \
 		--node-ip $NODE_IP \
-		--advertise-address $NODE_IP \
-		--tls-san $NODE_IP \
-		--flannel-iface eth1" \
-	sh -
+            	--advertise-address $NODE_IP \
+            	--tls-san $NODE_IP \
+            	--flannel-iface eth1" \
+        sh -
 
 until [ -f /var/lib/rancher/k3s/server/node-token ]; do
     sleep 3
 done
 
 sudo tee "$TOKEN_FILE" < /var/lib/rancher/k3s/server/node-token >/dev/null
+
