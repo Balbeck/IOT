c9fcfa891696214d2aaf0f3c3a707b2a2baa85c1
Hector <90897873+Balbeck@users.noreply.github.com>
2026-02-10 01:34:32 +0100

    Modify K3s agent installation script
    Updated the installation script for K3s agent mode to include necessary packages and configurations.

--- P1/scripts/install_K3s_agent_mode.sh
+++ P1/scripts/install_K3s_agent_mode.sh
@@ -1,2 +1,24 @@
-sudo apt-get update
-sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms
+#!/bin/bash
+
+NODE_IP_SERVER="192.168.56.110"
+TOKEN_FILE="/vagrant_shared/node-token"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y curl
+sudo apt-get install -y nfs-kernel-server
+# sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms
+
+until [ -f "$TOKEN_FILE" ]; do
+    sleep 3
+done
+
+K3S_TOKEN=$(cat "$TOKEN_FILE")
+
+curl -sfL https://get.k3s.io | \
+	K3S_URL="https://$NODE_IP_SERVER:6443" \
+	K3S_TOKEN="$K3S_TOKEN" \
+	INSTALL_K3S_EXEC="agent \
+		--node-ip 192.168.56.111 \
+        	--flannel-iface eth1" \
+    	sh -
