e3e05c8b54354e18ac89e0272dc0c370c257e6ef
balbecke <albecker.hector@gmail.com>
2026-02-25 17:08:01 +0100

    feat: ArgoCd install improvment

--- P3/scripts/create_cluster_and_install_argoCD_in_k3d.sh
+++ P3/scripts/create_cluster_and_install_argoCD_in_k3d.sh
@@ -6,7 +6,7 @@ create_namespace() {
     if ! kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
         kubectl create namespace "$NAMESPACE"
     fi
-    echo "Waiting for namespace $NAMESPACE to be Active..."
+    echo "⏳ Waiting for namespace $NAMESPACE to be Active..."
     kubectl wait --for=condition=Established --timeout=30s \
         namespace "$NAMESPACE" >/dev/null 2>&1 || true
     # Verification
@@ -21,34 +21,34 @@ set -e
 
 # Create Cluster
 if k3d cluster list | grep -q "iot"; then
-    echo "Cluster \'iot\' already exists!"
-    echo "deleting cluster \'iot\'..."
+    echo "🚫 Cluster [ iot ] already exists!"
+    echo "🗑️ Deleting cluster [ iot ]..."
     k3d cluster delete iot 2>/dev/null || true
 fi
-echo "Creating new k3d cluster..."
+echo "🏗️ Creating new k3d cluster..."
 k3d cluster create iot -p "80:80@loadbalancer" --agents 1
 
 # Waiting for cluster to be ready before going on
-echo "Waiting for cluster to be ready..."
+echo "⏳ Waiting for cluster to be ready..."
 until kubectl get nodes >/dev/null 2>&1; do
-    echo "Waiting for K8s API..."
+    echo "⏳ Waiting for K8s API..."
     sleep 2
 done
 echo "✅️ K8s API is ready !"
 
-echo "Waiting for nodes to be ready..."
+echo "⏳ Waiting for nodes to be ready..."
 while [[ $(kubectl get nodes --no-headers 2>/dev/null | awk '{print $2}') != "Ready" ]]; do
-    echo "Waiting for nodes to be on Ready state..."
+    echo "⏳ Waiting for nodes to be on Ready state..."
     sleep 2
 done
-echo "✅️ Cluster \'iot\' is Ready !"
+echo "✅️ Cluster [ iot ] is Ready !"
 
 # # Or simply...
 # kubectl wait --for=condition=Ready node --all --timeout=30s
 
 
 # Create namespaces
-echo "Creating namespaces..."
+echo "🏗️ Creating namespaces..."
 create_namespace argocd
 create_namespace dev
 
@@ -61,11 +61,11 @@ create_namespace dev
 
 
 # Install ArgoCD
-echo "Installing ArgoCD..."
+echo "🏗️ Installing ArgoCD..."
 kubectl apply -n argocd \
 -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "Waiting for Argo CD pods to be ready..."
+echo "⏳ Waiting for Argo CD pods to be ready..."
 kubectl wait --for=condition=available deployment --all -n argocd --timeout=300s
 echo "✅️ Argo CD installed successfully!"
 
